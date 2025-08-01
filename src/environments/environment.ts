f5ea8470e8388d7662124de48aac682271735bcf
moneysab <contact@moneysab.fr>
2025-08-01 02:51:50 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: 'http://ec2-13-39-196-2.eu-west-3.compute.amazonaws.com:8080',
-  epinApiUrl: 'http://ec2-52-47-41-115.eu-west-3.compute.amazonaws.com:8080/cardexis-settlement',
+  apiUrl: 'http://84.234.31.171:8080',
+  epinApiUrl: 'http://84.234.31.171:8080/cardexis-settlement',
 };
 
 /*
