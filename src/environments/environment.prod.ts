3daef912cbbc1c41bb3c22f8a0c6e81a1c3cd162
ElhassaneAhgoune <105168493+ElhassaneAhgoune@users.noreply.github.com>
2025-08-05 00:29:21 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://c-flow-api.moneysab.fr',
+  apiUrl: 'https://c-bill-api.moneysab.fr',
   epinApiUrl: 'http://84.234.31.171:8082/cardexis-settlement',
 };
