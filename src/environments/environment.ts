bf49430bc18bd7eb42929294f162f71513d4f4df
ElhassaneAhgoune <105168493+ElhassaneAhgoune@users.noreply.github.com>
2025-08-04 18:01:09 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: '/api',
+  apiUrl: '',
   epinApiUrl: 'http://84.234.31.171:8082/cardexis-settlement',
 };
 
